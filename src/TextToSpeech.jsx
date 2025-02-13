fc17bf2fc3717b855f2001962376a79ff7fb9a24
Thet Paing Oo <phoenixjames05@gmail.com>
2025-02-13 10:57:53 +0630

    change female voice for tts

--- src/TextToSpeech.jsx
+++ src/TextToSpeech.jsx
@@ -3,7 +3,15 @@ import { useSpeech, useVoices } from "react-text-to-speech";
 
 export default function TextToSpeech({ textData }) {
   const [text, setText] = useState(textData)
-  const { Text, speechStatus, start, pause, stop } = useSpeech({ text: text, lang: 'en-US', voiceURI: 'Microsoft Zira - English (United States)', autoPlay: true, highlightText: true, showOnlyHighlightedText: false, highlightMode: "word" });
+  const { Text, speechStatus, start, pause, stop } = useSpeech({
+    text: text,
+    lang: "en-GB",
+    voiceURI: "Google UK English Female",
+    autoPlay: true,
+    highlightText: true,
+    showOnlyHighlightedText: false,
+    highlightMode: "word"
+  });
 
   return (
     <div style={{ display: "flex", flexDirection: "column", rowGap: "1rem" }}>
